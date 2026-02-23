b5a404d7d0b4db5a747784cbf871c0fc32d0a0d0
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-02-23 15:30:00 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    string s;
+
+    while (getline(cin, s)) {
+        cout << s << '\n';
+    }
+}
