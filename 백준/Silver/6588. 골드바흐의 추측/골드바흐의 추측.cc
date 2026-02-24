35c925e94f10ec2b78f775b69d520814513c9791
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-02-24 16:42:58 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 48 ms, Memory: 3892 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr int MAXN = 1'000'000;
+bool isPrime[MAXN + 1];
+vector<int> primes;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    fill(isPrime, isPrime + MAXN + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i=2; i*i<=MAXN; ++i) {
+        if (!isPrime[i]) continue;
+        for (int j=i*i; j<=MAXN; j+=i) {
+            isPrime[j] = false;
+        }
+    }
+
+    for (int i=2; i<=MAXN; ++i) {
+        if (isPrime[i])
+            primes.push_back(i);
+    }
+
+    int n;
+    while (true) {
+        cin >> n;
+        if (n == 0)
+            break;
+        for (int prime: primes) {
+            if (prime > n / 2) {
+                cout << "Goldbach's conjecture is wrong.\n";
+                break;
+            }
+            if (isPrime[n - prime]) {
+                cout << n << " = " << prime << " + " << n - prime << '\n';
+                break;
+            }
+        }
+    }
+}
