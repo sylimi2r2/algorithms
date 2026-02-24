4f57f01b4e652dc89e15a7113c930f394dbf9568
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-02-24 16:02:45 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2260 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int M, N;
+    cin >> M >> N;
+
+    vector<bool> isPrime(N + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i=2; i*i<=N; ++i) {
+        if (!isPrime[i]) continue;
+        for (int j=i*i; j<=N; j+=i) {
+            isPrime[j] = false;
+        }
+    }
+
+    for (int i=M; i<=N; ++i) {
+        if (isPrime[i]) {
+            cout << i << '\n';
+        }
+    }
+}
