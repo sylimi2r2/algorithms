c0a435037a9f1b642c2858270a77919ed0350198
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-03-24 14:22:00 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+    vector<int> nums(N);
+    for (int i=0; i<N; ++i) {
+        cin >> nums[i];
+    }
+
+    int pivot = N - 2;
+    while (pivot >= 0 && nums[pivot] > nums[pivot + 1]) --pivot;
+
+    if (pivot == -1) {
+        cout << -1;
+        return 0;
+    }
+
+    int target = N - 1;
+    while (nums[target] < nums[pivot]) --target;
+
+    swap(nums[pivot], nums[target]);
+    reverse(nums.begin() + pivot + 1, nums.end());
+
+    for (int num: nums) {
+        cout << num << ' ';
+    }
+}
