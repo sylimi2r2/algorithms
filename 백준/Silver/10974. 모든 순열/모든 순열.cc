a89522e428ffaa64dc164a362d768e288ae71cb4
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-03-24 14:34:04 +0900

    [Silver III] Title: 모든 순열, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.cc
+++ 백준/Silver/10974. 모든 순열/모든 순열.cc
@@ -1,35 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-
-int N;
-int picked[8];
-bool visited[9];
-
-void dfs(int cnt) {
-    if (cnt == N) {
-        for (int i=0; i<N; ++i) {
-            cout << picked[i] << ' ';
-        }
-        cout << '\n';
-        return;
-    }
-
-    for (int i=1; i<=N; ++i) {
-        if (!visited[i]) {
-            visited[i] = true;
-            picked[cnt] = i;
-            dfs(cnt + 1);
-            visited[i] = false;
-        }
-    }
-}
 
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
+    int N;
+    vector<int> v;
     cin >> N;
+    for (int i=1; i<=N; ++i) {
+        v.push_back(i);
+    }
 
-    dfs(0);
+    do {
+        for (int x: v) {
+            cout << x << ' ';
+        }
+        cout << '\n';
+   } while (next_permutation(v.begin(), v.end()));
 }
