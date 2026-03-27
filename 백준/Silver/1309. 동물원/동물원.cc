9f253628cb756a73e9cc2b67053b7cbcc3716885
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-03-27 20:23:08 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,20 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
+const int MOD = 9901;
+
 int dp[100001];
-int N;
 
 int main() {
-    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
 
     dp[1] = 3;
     dp[2] = 7;
-
-    cin >> N;
-    for(int i=3; i<=N; i++) {
-        dp[i] = (2*dp[i-1]+dp[i-2]) % 9901;
+    for (int i=3; i<=N; ++i) {
+        dp[i] = (2 * dp[i - 1] + dp[i - 2]) % MOD;
     }
 
     cout << dp[N];
