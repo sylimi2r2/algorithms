4ccb85850b7c9e1bf8bb721650aacccd0ef87e7c
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-01 18:25:10 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,31 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
 
-    int triangle[501][501] = {0, };
-    int N, sol = 0;
-    cin >> N;
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=i; j++) {
-            cin >> triangle[i][j];
-            if(j!=1 && j!=i)
-                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1]);
-            else if(j == 1)
-                triangle[i][j] += triangle[i-1][j];
-            else
-                triangle[i][j] += triangle[i-1][j-1];
+    int n;
+    cin >> n;
+
+    vector<int> prev(n + 1, 0);
+    cin >> prev[1];
+    for (int i=2; i<=n; ++i) {
+        vector<int> cur(i + 1);
+        for (int j=1; j<=i; ++j) {
+            cin >> cur[j];
         }
+
+        prev[i] = prev[i - 1] + cur[i];
+        for (int j=i-1; j>1; --j) {
+            prev[j] = max(prev[j-1], prev[j]) + cur[j];
+        }
+        prev[1] += cur[1];
     }
 
-    for(int i=1; i<=N; i++) {
-        sol = max(sol, triangle[N][i]);
-    }
-
-    cout << sol;
-
+    cout << *max_element(prev.begin() + 1, prev.begin() + n + 1);
 }
