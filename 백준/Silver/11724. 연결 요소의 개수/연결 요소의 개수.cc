4af35a39067d707d0e9aa6a4181773e998190946
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-02 20:41:15 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 68 ms, Memory: 6532 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,47 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+
 using namespace std;
 
-vector<int> edges[1001];
-bool visited[1001] = {0, };
-void bfs(int v) {
-    queue<int> q;
-    q.push(v);
-    visited[v] = true;
-    while(!q.empty()) {
-        int current = q.front();
-        q.pop();
-        for(int &next : edges[current]) {
-            if(!visited[next]) {
-                q.push(next);
-                visited[next] = true;
-            }
+int ans = 0;
+int N, M;
+vector<int> graph[1001];
+bool visited[1001];
+
+void dfs(int cur) {
+    for (int next: graph[cur]) {
+        if (!visited[next]) {
+            visited[next] = true;
+            dfs(next);
         }
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
-    int N, M;
     cin >> N >> M;
-    while(M--) {
+
+    for (int i=0; i<M; ++i) {
         int u, v;
         cin >> u >> v;
-        edges[u].push_back(v);
-        edges[v].push_back(u);
+
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
 
-    int cnt = 0;
-    for(int i=1; i<=N; i++) {
-        if(!visited[i]) {
-            cnt++;
-            bfs(i);
+    for (int i=1; i<=N; ++i) {
+        if (!visited[i]) {
+            ++ans;
+            visited[i] = true;
+            dfs(i);
         }
     }
 
-    cout << cnt;
+    cout << ans;
+
 }
