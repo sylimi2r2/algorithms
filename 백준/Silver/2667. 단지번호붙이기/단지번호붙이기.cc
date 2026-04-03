b46d5d77a0d635ff0ce53e5f50de8bf8db671b74
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-03 15:28:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,65 +1,61 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
+#include <vector>
 #include <queue>
+#include <algorithm>
+
 using namespace std;
 
-int map[25][25] = {0,};
-bool visited[25][25] = {0,};
-pair<int, int> ways[4] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-int N;
-
-int BFS(int x, int y) {
-    int sum = 0;
-    queue<pair<int,int>> q;
-    q.push({x, y});
-    visited[x][y] = true;
-    sum++;
-    while(!q.empty()) {
-        pair<int, int> current = q.front();
-        q.pop();
-        for(pair<int, int> way : ways) {
-            int nextX = current.first + way.first;
-            int nextY = current.second + way.second;
-            if(nextX>=0 && nextX<N && nextY>=0 && nextY<N) {
-                if(map[nextX][nextY] && !visited[nextX][nextY]) {
-                    q.push({nextX, nextY});
-                    visited[nextX][nextY] = true;
-                    sum++;
-                }
-            }
-        }
-    }
-    return sum;
-}
+string board[25];
+bool visited[25][25];
+pair<int, int> dir[4] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
-    vector<int> solution;
+    int N;
     cin >> N;
-    for(int i=0; i<N; i++) {
-        string str;
-        cin >> str;
-        for(int j=0; j<N; j++) {
-            map[i][j] = str[j] - '0';
-        }
+    
+    for (int i=0; i<N; ++i) {
+        cin >> board[i];
     }
 
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            if(map[i][j] && !visited[i][j]) {
-                int sol = BFS(i, j);
-                solution.push_back(sol);
+    vector<int> cnts;
+    queue<pair<int, int>> q;
+    
+    for (int i=0; i<N; ++i) {
+        for (int j=0; j<N; ++j) {
+            if (board[i][j] == '0' || visited[i][j]) continue;
+
+            int cnt = 0;
+            visited[i][j] = true;
+            q.push({i, j});
+            
+            while (!q.empty()) {
+                auto [x, y] = q.front();
+                q.pop();
+                ++cnt;
+
+                for (auto [dx, dy]: dir) {
+                   int nx = x + dx;
+                   int ny = y + dy;
+
+                    if (nx < 0 || nx >= N || ny < 0 || ny >=N) continue;
+                    if (board[nx][ny] == '0' || visited[nx][ny]) continue;
+
+                    visited[nx][ny] = true;
+                    q.push({nx, ny});
+                }
             }
+
+            cnts.push_back(cnt);
         }
     }
 
-    sort(solution.begin(), solution.end());
-    cout << solution.size() << '\n';
-    for(int sol : solution) {
-        cout << sol << '\n';
+    sort(cnts.begin(), cnts.end());
+
+    cout << cnts.size() << '\n';
+    for (int cnt: cnts) {
+        cout << cnt << '\n';
     }
 }
