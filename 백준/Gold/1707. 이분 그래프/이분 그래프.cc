84684e69e5812c1ec72732a6974271f52daef61f
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-03 14:46:07 +0900

    [Gold IV] Title: 이분 그래프, Time: 240 ms, Memory: 5860 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int state[20001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int K;
+    cin >> K;
+
+    while (K--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<vector<int>> graph(V + 1);
+
+        for (int i=1; i<=V; ++i) {
+            state[i] = 0;
+        }
+
+        for (int i=0; i<E; ++i) {
+            int u, v;
+            cin >> u >> v;
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        bool isBipartite = true;
+        queue<int> q;
+        for (int i=1; i<=V && isBipartite; ++i) {
+            if (state[i] == 0) {
+                state[i] = 1;
+                q.push(i);
+
+                while (!q.empty() && isBipartite) {
+                    int cur = q.front();
+                    q.pop();
+
+                    for (int next: graph[cur]) {
+                        if (state[next] == 0) {
+                            state[next] = -state[cur];
+                            q.push(next);
+                            continue;
+                        }
+
+                        if (state[next] == state[cur]) {
+                            isBipartite = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        cout << (isBipartite ? "YES" : "NO") << '\n';
+    }
+}
