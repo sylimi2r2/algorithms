a05853bcf009450e37f77913a75d55c0fa09dde7
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-03 19:27:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2384 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,47 +1,44 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
-int N, K;
-bool visited[100001] = {0,};
+struct State {
+    int cur, t;
+};
 
-void bfs() {
-    queue<pair<int, int>> q;
-    q.push({N, 0});
-    visited[N] = true;
-
-    while(!q.empty()) {
-        int cur = q.front().first;
-        int curCost = q.front().second;
-        q.pop();
-        if(cur == K) {
-            cout << curCost;
-            break;
-        }
-        int next1 = cur+1;
-        int next2 = cur-1;
-        int next3 = cur*2;
-        if(next1<=100000 && !visited[next1]) {
-            q.push({next1, curCost+1});
-            visited[next1] = true;
-        }
-        if(next2>=0 && !visited[next2]) {
-            q.push({next2, curCost+1});
-            visited[next2] = true;
-        }
-        if(next3<=100000 && !visited[next3]) {
-            q.push({next3, curCost+1});
-            visited[next3] = true;
-        }
-    }
-}
+bool visited[100001];
 
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
+    int N, K;
     cin >> N >> K;
 
-    bfs();
+    if (N >= K) {
+        cout << N - K;
+        return 0;
+    }
+
+    queue<State> q;
+    q.push({N, 0});
+
+    while (!q.empty()) {
+        auto [cur, t] = q.front();
+        q.pop();
+
+        if (cur == K) {
+            cout << t;
+            break;
+        }
+
+        int next[3] = {cur + 1, cur - 1, cur * 2};
+        for (int n: next) {
+            if (n < 0 || n > 100000 || visited[n]) continue;
+
+            visited[n] = true;
+            q.push({n, t + 1});
+        }
+    }
 }
