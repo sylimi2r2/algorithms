e8cb11805b14597b785c3cedf2247d5ee54eb624
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-03 15:52:32 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,71 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <cmath>
+
 using namespace std;
 
-vector<vector<int>> map;
-int N, M;
+struct Node {
+    int x, y, d;
+};
 
-void solution(int xPos, int yPos) {
-    priority_queue<vector<int>> pq;
-    vector<pair<int, int>> way = {{0,  1},
-                                  {0,  -1},
-                                  {1,  0},
-                                  {-1, 0}};
-
-    int MC = 0;
-    int EC = (int) sqrt(pow(N, 2) + pow(M, 2));
-    int TC = MC + EC;
-
-    vector<int> firstNode = {-TC, -MC, xPos, yPos};
-    pq.push(firstNode);
-
-    while (!pq.empty()) {
-        while (true) {
-            TC = -pq.top()[0];
-            MC = -pq.top()[1];
-            xPos = pq.top()[2];
-            yPos = pq.top()[3];
-            pq.pop();
-            if (map[xPos][yPos]) break;
-        }
-        map[xPos][yPos] = 0;
-        if (xPos == N && yPos == M) break;
-        for (int i = 0; i < 4; i++) {
-            int nextX = xPos + way[i].first;
-            int nextY = yPos + way[i].second;
-            if (nextX >= 0 && nextX <= N && nextY >= 0 && nextY <= M && map[nextX][nextY]) {
-                int nextMC = MC + 1;
-                int nextEC = (int) sqrt(pow(N - nextX, 2) + pow(M - nextY, 2));
-                int nextTC = nextMC + nextEC;
-                vector<int> nextNode = {-nextTC, -nextMC, nextX, nextY};
-                pq.push(nextNode);
-            }
-        }
-    }
-    cout << TC+1;
-}
+string board[100];
+bool visited[100][100];
+pair<int, int> dir[4] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int N, M;
     cin >> N >> M;
 
-    for(int i=0; i<N; i++) {
-        vector<int> mapI;
-        string s;
-        cin >> s;
-        for(int j=0; j<M; j++) {
-            mapI.push_back((int)(s[j] - '0'));
-        }
-        map.push_back(mapI);
+    for (int i=0; i<N; ++i) {
+        cin >> board[i];
     }
 
-    N--;
-    M--;
+    queue<Node> q;
+    visited[0][0] = true;
+    q.push({0, 0, 1});
 
-    solution(0,0);
+    while (!q.empty()) {
+        auto [x, y, d] = q.front();
+        q.pop();
 
+        if (x == N - 1 && y == M - 1) {
+            cout << d;
+            break;
+        }
+
+        for (auto [dx, dy]: dir) {
+            int nx = x + dx;
+            int ny = y + dy;
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (board[nx][ny] == '0' || visited[nx][ny]) continue;
+
+            visited[nx][ny] = true;
+            q.push({nx, ny, d + 1});
+        }
+    }
 }
