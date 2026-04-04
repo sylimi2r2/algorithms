c22f458743a7a12ec000bf2369bfdbd9fdb09884
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-04 22:26:00 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.04 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,47 +1,39 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
+vector<vector<int>> graph;
+vector<int> subSize;
+
+int dfs(int cur, int parent) {
+    int &ret = subSize[cur];
+    
+    ret = 1;
+    
+    for (int next: graph[cur]) {
+        if (next == parent) continue;
+        ret += dfs(next, cur);
+    }
+    
+    return ret;
+}
+
 int solution(int n, vector<vector<int>> wires) {
-    int answer = n;
+    graph.assign(n + 1, {});
+    subSize.assign(n + 1, 0);
     
-    vector<vector<int>> graph(n + 1);
-    
-    for (auto &wire: wires) {
-        int v1 = wire[0];
-        int v2 = wire[1];
-        
+    for (auto& wire: wires) {
+        int v1 = wire[0], v2 = wire[1];
         graph[v1].push_back(v2);
         graph[v2].push_back(v1);
     }
     
-    for (auto &wire: wires) {
-        int cut1 = wire[0];
-        int cut2 = wire[1];
-        
-        int cnt = 0;
-        vector<bool> visited(n + 1, false);
-        queue<int> q;
-        visited[1] = true;
-        q.push(1);
-        
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            ++cnt;
-            
-            for (int next: graph[cur]) {
-                if ((cur == cut1 && next == cut2) || (cur == cut2 && next == cut1)) continue;
-                if (visited[next]) continue;
-                
-                visited[next] = true;
-                q.push(next);
-            }
-        }
-        
-        answer = min(answer, abs(n - 2 * cnt));
+    dfs(1, 0);
+    
+    int answer = n;
+    for (int node=2; node<=n; ++node) {
+        answer = min(answer, abs(n - 2 * subSize[node]));
     }
     
     return answer;
