1579d832a5298d894a3dd1034866c926c9e1f4c0
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-13 16:27:46 +0900

    [level 1] Title: 폰켓몬, Time: 0.81 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -6,5 +6,5 @@ int solution(vector<int> nums)
 {
     unordered_set<int> s(nums.begin(), nums.end());
     
-    return min(s.size(), nums.size() / 2);
+    return min(nums.size() / 2, s.size());
 }
