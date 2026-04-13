e6b150c5d28aae5311845686a60488f06d23a943
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2026-04-13 15:41:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 37.42 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -7,7 +7,7 @@ using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
     unordered_map<string, int> m;
     
-    for (string c: completion) {
+    for (string& c: completion) {
         auto it = m.find(c);
         
         if (it == m.end()) {
@@ -17,17 +17,17 @@ string solution(vector<string> participant, vector<string> completion) {
         }
     }
     
-    for (string p: participant) {
+    for (string& p: participant) {
         auto it = m.find(p);
         
         if (it == m.end()) {
             return p;
         }
-        
-        if (it != m.end()) {
-            if (--it->second < 0) {
-                return p;
-            }
+
+        if (it->second > 0) {
+            --it->second;
+        } else {
+            return p;
         }
     }
 }
